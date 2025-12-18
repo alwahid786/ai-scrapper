bdbefaa653364cb4982e58c52e7bcbd715a8e64f
Rabbiah5 <rabbiahqamar5@gmail.com>
2025-12-18 15:56:32 +0500

    push cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:5175',
-      'https://ai-scrapper-72jb.onrender.com/api',
+      'https://real-state-frontend-umber.vercel.app',
     ],
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
     credentials: true,
